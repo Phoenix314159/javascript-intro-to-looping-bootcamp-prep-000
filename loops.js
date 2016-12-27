edeb473efb1b2334ff20084d0465ff3e8e2b8fc7
James Langford <derivative7117@live.com>
2016-12-27 03:06:40 +0000

    Done.

--- loops.js
+++ loops.js
@@ -1,7 +1,7 @@
 function forLoop(array){
 
   for (var i = 0; i < 25; i++){
-    if (i = 1){
+    if (i == 1){
       array.push(`I am 1 strange loop`);
     }
     else {
@@ -9,3 +9,21 @@ function forLoop(array){
     }
   }   return array;
  }
+
+function whileLoop(n){
+  while(n>0){
+    console.log(--n)
+  }
+  return 'done';
+}
+
+function doWhileLoop(array){
+  function maybeTrue() {
+           return Math.random() >= 0.5
+     }
+  do{
+    array.pop();
+
+  }while (maybeTrue());
+  return array;
+}
